1b39b1ccdea2470bb69ad5cd94f7309658db55e3
SitiUlunNuhaKarimah127 <200411100127@student.trunojoyo.ac.id>
2024-06-28 20:58:55 +0700

    update UI

--- function.py
+++ function.py
@@ -2,6 +2,7 @@ from nltk.tokenize import sent_tokenize
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from nltk.tokenize import word_tokenize
 import streamlit as st
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -11,33 +12,32 @@ import nltk
 nltk.download('stopwords')
 nltk.download('punkt')
 
-def cleaning(text):
-  text = re.sub(r'[^\w\s.?!,]', '', text).strip().lower()
+# Preprocessing Summarization
+def case_folding(text):
+  text = text.lower()
   return text
 
-corpus = stopwords.words('indonesian')
-def stopwords(text):
-  return ' '.join(word for word in text.split() if word not in corpus)
+def cleaning(text):
+  text = re.sub(r'[^\w\s.,]', '', text).strip().lower()
+  return text
 
-def tokenizer(text):
-  text = text.lower()
+def tokenisasi(text):
   return sent_tokenize(text)
 
-def plot_graph(G, figsize=(35, 30), node_size=700, node_color='skyblue'):
-  # Menggambar graf dengan canvas yang diperbesar
-  pos = nx.spring_layout(G)  # Menentukan posisi simpul
-  labels = nx.get_edge_attributes(G, 'weight')
+# Preprocessing Klasifikasi
+def cleaning_text_klasifikasi(text):
+  text = re.sub(r'[^a-zA-Z\s]', '', text).strip()
+  return text
 
-  # Menentukan ukuran canvas
-  fig = plt.figure(figsize=figsize)
+def tokenisasi_klasifikasi(text):
+  return word_tokenize(text)
 
-  # Menggambar graf dengan ukuran canvas yang diperbesar
-  nx.draw(G, pos, with_labels=True, node_size=node_size, node_color=node_color)
-  nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_color='red')
-  # plt.show()
-  st.pyplot(fig)
 
-def graph_cosine_sim(x, threshold = 0.11):
+def stopwordText_klasifikasi(words):
+  corpus = stopwords.words('indonesian')
+  return ' '.join([word for word in words if word not in corpus])
+
+def graph_cosine_sim(x, threshold=0.1):
   # TFIDF
   vectorizer = TfidfVectorizer()
   tfidf = vectorizer.fit_transform(x)
@@ -45,31 +45,27 @@ def graph_cosine_sim(x, threshold = 0.11):
   G = nx.Graph()
 
   # Mengisi nilai similarity antara kalimat ke dalam edges (Garis Penghubung)
-  for i in range(len(x)):
-    for j in range(i+1, len(x)):
-      sim = cos_sim[i][j]
+  for kalimat in range(len(x)):
+    for kalimat_sim in range(kalimat+1, len(x)):
+      sim = cos_sim[kalimat][kalimat_sim]
       if sim > threshold:
-        G.add_edge(i, j, weight=sim)
-
+        G.add_edge(kalimat, kalimat_sim, weight=sim)
   return G
 
-def summarization(x, k = 4, threshold=0.11):
-  st.session_state.tokenSentence = x
+def summarization(x, k = 10, threshold=0.1):
   
-  # Memasukkan Nilai Cosine Similirity ke dalam Graph
+  # Memasukkan Nilai Cosine Similarity ke dalam Graph
   G = graph_cosine_sim(x, threshold)
 
-  # Menghitung nilai dari closeness centrality
-  centrality = nx.closeness_centrality(G)
-  
-  st.session_state.centrality = centrality
+  # Menghitung nilai dari Textrank
+  score = nx.pagerank(G)
 
-  # Menyusun Kalimat berdasarkan nilai Closeness Centrality tertinggi dan lebih dari treshold
-  centrality = dict(sorted(centrality.items(), key=lambda item : item[1], reverse=True))
+  # Menyusun Kalimat berdasarkan nilai textrank tertinggi
+  score = dict(sorted(score.items(), key=lambda item : item[1], reverse=True))
 
   summary_sentences = []
-  for i, centr in enumerate(centrality.items()):
-    if i < k:
+  for kalimat, centr in enumerate(score.items()):
+    if kalimat < k:
       summary_sentences.append(x[centr[0]])
 
-  return ('_' + ' '.join(summary_sentences)+ '_', G)
+  return (' '.join(summary_sentences), G)
--- main.py
+++ main.py
@@ -1,71 +1,27 @@
-from streamlit_option_menu import option_menu
 from function import *
 import joblib
 
 st.header("Klasifikasi Artikel Berita Berdasarkan Hasil Summarization", divider='rainbow')
 
 text = st.text_area("Masukkan Artikel Berita")
-col1, col2 = st.columns(2)
-with col1:
-    k = st.number_input("Jumlah Kalimat Yang Di Ambil", 1)
-
-with col2:
-    threshold = st.number_input("Threshold")
 
 button = st.button("Submit")
 
-if "summary" not in st.session_state:
-    st.session_state.summary = []
-    st.session_state.graph_klasifikasi = []
-    st.session_state.klasifikasi = ""
-
 if button:
-  text_clean = cleaning(text)
-  tokenizing = tokenizer(text_clean)
-  summary, G = summarization(x=tokenizing, k=k, threshold=threshold)
+  casefolding = case_folding(text)
+  text_clean = cleaning(casefolding)
+  tokenizing = tokenisasi(text_clean)
+  summary, G = summarization(x=tokenizing, k=10, threshold=0.1)
   
-  st.session_state.summary = summary
-  st.session_state.graph_klasifikasi = G
-
-
-selected = option_menu(
-  menu_title="",
-  options=["Summary", "Klasifikasi", "Graph Kalimat"],
-  icons=["data", "Process", "model", "implemen", "Test", "sa"],
-  orientation="horizontal"
-  )
-
-if selected == "Summary":
-  if st.session_state.summary:
-    sentence = st.session_state.tokenSentence
-    closeness = st.session_state.centrality
-    st.write("**Hasil Summarization:**")
-    st.write(st.session_state.summary)
-    st.write("Nilai Closeness Centrality :")
-    for i, cls in enumerate(closeness):
-      st.write(f"index {i} Closeness : {closeness[cls]} -> Kalimat : {sentence[i]}")
+  st.write("**Hasil Summarization:**")
+  st.write(summary)
+  text_clean_klasifikasi = cleaning_text_klasifikasi(summary)
+  tokenizing_klasifikasi = tokenisasi_klasifikasi(text_clean_klasifikasi)
+  stopword_removal_klasifikasi = stopwordText_klasifikasi(tokenizing_klasifikasi)
+  st.caption("Klasifikasi Berdasarkan Hasil Summarization (SVM)")
+  vectorizer = joblib.load("resources/vectorizer_summary.pkl")
+  model = joblib.load("resources/Best Model Dengan Peringkasan.pkl")
   
-  
-elif selected == "Klasifikasi":
-  if st.session_state.summary:
-      st.caption("Klasifikasi Berdasarkan Hasil Summarization (Naive Bayes)")
-      new_text = st.session_state.summary
-      vectorizer = joblib.load("resources/vectorizer_summary.pkl")
-      nb = joblib.load("resources/modelNB_summary.pkl")
-      new_text_matrics = vectorizer.transform([new_text]).toarray()
-      prediction = nb.predict(new_text_matrics)
-      st.write("Prediction Category : ", prediction[0])
-     
-
-elif selected == "Graph Kalimat":
-  col1, col2 = st.columns(2)
-  with col1:
-    x_canvas = st.number_input('Lebar Canvas', 5)
-  
-  with col2:
-    y_canvas = st.number_input('Panjang Canvas', 5)
-    
-  node_size = st.number_input('Node Size', 400)
-  
-  if st.session_state.graph_klasifikasi != []:
-    plot_graph(st.session_state.graph_klasifikasi, (x_canvas, y_canvas), node_size)
+  new_text_matrics = vectorizer.transform([stopword_removal_klasifikasi]).toarray()
+  prediction = model.predict(new_text_matrics)
+  st.write("Prediction Category : ", prediction[0])
